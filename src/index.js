7b7b25f555c16f471fe5bd2f13a4c26ea1c309d4
qgarnaldo <qqgarnaldo@tip.edu.ph>
2022-01-20 15:10:09 +0800

    Initialize project using Create React App

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import ReactDOM from "react-dom";
+
+
+//My first component
+
+function HelloWorld() {
+	return <h1>Hello, World!</h1>;
+}
+
+ReactDOM.render(<HelloWorld></HelloWorld>, document.getElementById("root"));
